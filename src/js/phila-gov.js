ae15603d830db8f95270e567730afa2e7cee231b
Karissa Demi <k@karissademi.com>
2017-03-23 17:11:34 -0400

    External sites will never need items in the global nav marked as current

--- src/js/phila-gov.js
+++ src/js/phila-gov.js
@@ -14,16 +14,6 @@ module.exports = $(function(){
 
   var translate = setTimeout(function() { $('#google_translate_element a').prepend('<i class="fa fa-globe"></i>'); }, 1000);
 
-  $( $( '.global-nav a' ) ).each( function() {
-    if ( currentPath == $( this ).attr('href') || currentPath == $( this ).data( 'link' ) ){
-
-      $(this).addClass('js-is-current');
-      //special handling for services
-    }else if( currentPath.indexOf('/services/') === 0 ){
-      $('.services-menu-link a').addClass('js-is-current');
-    }
-  });
-
   /* Provide option for explict show/hide declarations, with jQuery fallbacks for older (ios) browsers */
   function togglePageBody( show ){
     if( show === true ){
