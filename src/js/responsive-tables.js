93f9b57b7de429b842111aa3764ce975db3957fe
Karissa Demi <k@karissademi.com>
2017-06-29 11:05:23 -0400

    Add option to hide empty columns

--- src/js/responsive-tables.js
+++ src/js/responsive-tables.js
@@ -33,18 +33,34 @@ module.exports = $(function(){
           //put tds into object so we can inject titleLables
           if ( tds.innerText ) {
             tableCell = $( tds[ k ] );
+            //get the text of the table headings
+            text = th.textContent || th.innerText || "";
           }else{
             tableCell = $( tds[ '' ] );
+            text = '';
           }
-
-          //get the text of the table headings
-          text = th.textContent || th.innerText || "";
 
           //prepend td with the heading
           td = tableCell.prepend( titleLables( text ) );
         }
       }
     }
+  }
+
+  var hiddenCols = $( "table.js-hide-empty" );
+
+  if ( hiddenCols.length > 0 ) {
+    $("th", hiddenCols).each(function(i) {
+      var remove = 0;
+      var tds = $(this).parents('table').find('tr td:nth-child(' + (i + 1) + ')');
+      tds.each(function(j) {
+        if (this.innerHTML == '') remove++;
+      });
+      if ( remove == ($('tr', hiddenCols).length - 1) ) {
+        $(this).hide();
+        tds.hide();
+      };
+    });
   }
 
 });
