d3da1871e2af7c55fb5389261b17f32e34f67df6
Karissa Demi <k@karissademi.com>
2017-08-14 14:21:31 -0400

    Use foundation responseive tables

--- src/js/main.js
+++ src/js/main.js
@@ -5,5 +5,4 @@ var philaFeedback = require('./feedback');
 var philaFeedbackToggle = require('./feedback-toggle');
 var philaBackToTop = require('./back-to-top');
 var philaGov = require('./phila-gov');
-var philaTables = require('./responsive-tables');
 var philaSVG = require('./svg-fallback');
