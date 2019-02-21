c0c496ec3e04c995e45039a47ce72ebac79cc5bc
Karissa <k@karissademi.com>
2019-02-21 12:48:27 -0500

    Remove phila.gov-specific js

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,6 @@ module.exports = require('foundation-sites');
 var philaFeedback = require('./feedback');
 var philaFeedbackToggle = require('./feedback-toggle');
 var philaBackToTop = require('./back-to-top');
-var philaGov = require('./phila-gov');
+//var philaGov = require('./phila-gov');
 var philaSVG = require('./svg-fallback');
 var philaTables = require('./responsive-tables')
